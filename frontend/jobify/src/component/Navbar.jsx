51da2c21e8fb89cddc94a32d55a8ab1704d2c268
Bhau2171 <survebhaumik694@gmail.com>
2025-03-21 15:05:36 +0530

    change border

--- frontend/jobify/src/component/Navbar.jsx
+++ frontend/jobify/src/component/Navbar.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import logo from '../assets/Screenshot 2025-03-21 005904.png';
 const Navbar = () => {
   return (
-    <div className='flex justify-between items-center p-5 bg-[#0A1A1A] text-[#f8fdff]'>
+    
+    <div className='flex justify-between items-center p-5 bg-[#0A1A1A] text-[#f8fdff] border-b-[1px] border-white'>
       <div className='flex gap-[10px] ml-5 items-center'>
         {/* Corrected Logo Image */}
-        <img src={logo} alt="Jobify Logo" className="w-[40px] h-[40px]" />
+        <img src={logo} alt="Jobify Logo" className="w-[40px] h-[40px] rounded-full" />
 
-        <div className='ml-6 font-semibold'><h2>Home</h2></div>
+        <div className='ml-6 font-semibold transition duration-300 hover:text-gray-400'><h2 className="relative pb-1 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-blue-400 after:transition-all after:duration-300 group-hover:after:w-full">Home</h2></div>
         <div className='ml-6 font-semibold'><h2>Job Trend</h2></div>
         <div className='ml-6 font-semibold'><h2>Skill Roadmap</h2></div>
         <div className='ml-6 font-semibold'><h2>My Progress</h2></div>
